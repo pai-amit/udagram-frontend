0158527a40d529ef4f2ec989e80f007c7fee5033
pai-amit <66574170+pai-amit@users.noreply.github.com>
2020-06-15 22:43:51 -0400

    Update environment with ApiHost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aba07ea2ce41743d4993a3aaead4aa81-956250105.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
